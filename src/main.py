a25260af02bbf6858dede9c1a849f7540aa21ae4
Arthur Murilo <arthur.msc1811@hotmail.com>
2026-05-05 20:53:44 -0300

    New changes

--- src/main.py
+++ src/main.py
@@ -1,15 +1,25 @@
 from langgraph.graph import StateGraph, MessagesState, START, END
 from typing import cast
+from rich import print
 
 def mock_llm(state: MessagesState):
-    return {"messages": [{"role": "ai", "content": "Iai tudo bem?"}]}
+    return {"messages": [{"role": "ai", "content": 'Teste2'}]}
+
+def verification(state: MessagesState):
+    messages = state["messages"]
+    if messages[-1].content == 'Teste1':
+        return {"messages": [{"role": "ai", "content": 'Funcionou'}]}
+    else:
+        return {"messages": [{"role": "ai", "content": 'Deu problema'}]}
 
 graph = StateGraph(MessagesState)
 graph.add_node(mock_llm)
+graph.add_node(verification)
 graph.add_edge(START, "mock_llm")
-graph.add_edge("mock_llm", END)
+graph.add_edge('mock_llm', 'verification')
+graph.add_edge("verification", END)
 graph = graph.compile()
 
 response = graph.invoke(cast(MessagesState,{"messages": [{"role": "user", "content": "hi!"}]}))
 
-print(response['messages'][1].content)
+print(response)
