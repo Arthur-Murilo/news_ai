a6bd89846f1cba19ac6943eba48f160c7ac4efd5
Arthur Murilo <arthur.msc1811@hotmail.com>
2026-05-09 18:42:19 -0300

    Atualiza a função de busca para incluir parâmetros dinâmicos de data e dias anteriores

--- src/agents/tools/search_tool.py
+++ src/agents/tools/search_tool.py
@@ -1,13 +1,16 @@
 from tavily import TavilyClient
 from langchain.tools import tool
+from datetime import datetime, timedelta
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
+before_days = int(os.getenv("BEFORE_DAYS", "7"))
+
 client = TavilyClient(os.getenv("TAVILY_API_KEY"))
 @tool
-def search_new(query: str) -> dict:
+def search_new(query: str, today: datetime=datetime.now(), before_days: int = before_days ) -> dict:
     """
     Realiza buscas avançadas na internet via Tavily para obter informações atualizadas e detalhadas.
     Ideal para pesquisas de notícias, artigos e contextos recentes, com foco em resultados do Brasil.
@@ -19,8 +22,8 @@ def search_new(query: str) -> dict:
         query=query,
         search_depth="advanced",
         max_results=5,
-        start_date="2026-04-07",
-        end_date="2026-05-07",
+        start_date=datetime.now().strftime("%Y-%m-%d"),
+        end_date=(today - timedelta(days=before_days)).strftime("%Y-%m-%d"),
         include_raw_content="text",
         country="brazil"
     )
