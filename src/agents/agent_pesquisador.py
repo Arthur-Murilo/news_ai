2df8565c9c8f74b7256ccd20078e1917b142687c
Arthur-Murilo <arthur.msc1811@hotmail.com>
2026-05-10 08:42:26 -0300

    Atualiza a configuração do modelo AI e refatora a inicialização do agente para usar a variável de ambiente

--- src/agents/agent_pesquisador.py
+++ src/agents/agent_pesquisador.py
@@ -3,12 +3,20 @@ from langchain.agents import create_agent
 from src.prompts.agent_pesquisador_prompt import get_system_prompt
 from src.agents.tools.search_tool import search_new
 from rich import print 
+from langchain_google_genai import ChatGoogleGenerativeAI
+import os
 
 load_dotenv()
 
+model = ChatGoogleGenerativeAI(
+    model = os.getenv("AI_MODEL", "gemini-2.5-flash"),
+    temperature = 0.3,
+    max_tokens = 1000
+)
+
 def call_agent(pergunta: str) -> str:
     agent = create_agent(
-        model="google_genai:gemini-2.5-flash",
+        model=model,
         tools=[search_new],
         system_prompt=get_system_prompt(),
     )
